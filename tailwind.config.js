0794c5c37872d3497ea7f218c3410fc5a8d692d2
Aluxian2130 <108323519+Aluxian2130@users.noreply.github.com>
2025-07-10 16:34:02 -0500

    Initial Commit
    Tailwind CSS Testing and layout implemented.

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class', // Enable class-based dark mode
+  content: ["./*.html"], // Scan all HTML files in your root folder
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  
+};
+
+
